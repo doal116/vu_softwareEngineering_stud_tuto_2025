a957895408bdd50f613fd5d92a5b5df352953f83
Alex Bulganin <bulganinbralex@yahoo.com>
2025-04-07 15:47:59 -0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,16 +3,8 @@
 
 using namespace std;
 
-int Sean(int a, int b){
-
-    return a + b;
-}
-
 int main()
 {
     cout << "hello world whats up :)" << endl;
     return 0;
-}
-int mahi(int a , int b){
-return a+b;
 }
