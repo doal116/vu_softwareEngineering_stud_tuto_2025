f3b961fce975f73d8b40f7c1534ab4a061b2caed
niyomalrw <niyomalrandula@gmail.com>
2025-04-07 17:59:40 +0300

    bcd

--- main.cpp
+++ main.cpp
@@ -8,3 +8,12 @@ int main()
     cout << "hello world what up :niyomal)" << endl;
     return 0;
 }
+def funtion123()
+{
+    cout << "hello world what up :niyomal)" << endl;
+}int a = 1;
+int b = 2;
+int c;
+a+b = c;
+return 0;
+} 
