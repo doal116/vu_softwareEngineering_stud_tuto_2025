67aed4c5760ae67ca1db30ab01dab2b7de467d07
Fuad Mahmud Shad <fuademrite@gmail.com>
2025-04-07 18:10:25 +0300

    Fuad's Chnages

--- main.cpp
+++ main.cpp
@@ -8,12 +8,7 @@ int main()
     cout << "hello world what up :niyomal)" << endl;
     return 0;
 }
-def funtion123()
-{
-    cout << "hello world what up :niyomal)" << endl;
-}int a = 1;
-int b = 2;
-int c;
-a+b = c;
-return 0;
-} 
+
+int fuad (int a, int b) {
+    return a + b;
+}
