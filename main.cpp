335b9eb63438e6beac7a99e6ecdaa5db105d4f1a
niyomalrw <niyomalrandula@gmail.com>
2025-04-09 20:59:02 +0300

    cal

--- main.cpp
+++ main.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
 
 using namespace std;
 
-int main()
+struct calculator
 {
-    cout << "hello 123" << endl;
+    int a=1;
+    int b=2;
+    int add {
+        add=a+b
+
+    }
+    int sub {
+        sub=a-b
+
+    }
+    int mul {
+        mul=a*b
+
+    }
+}
+int main()
+
+{
+    calculator att1;
+    cout << "add " << att1.add << endl;
+    cout << "sub " << att1.sub << endl;
+    cout << "mul " << att1.mul << endl;
     return 0;
 }
