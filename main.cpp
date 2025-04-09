e6321083e7f39e94d4b96a9cf17ff79ce6c3f0a6
doal116 <bulganinbralex@yahoo.com>
2025-04-09 21:06:25 -0100

    forgotten return statment

--- main.cpp
+++ main.cpp
@@ -9,25 +9,26 @@ struct calculator
 {
     int a=1;
     int b=2;
-    int add {
-        add=a+b
+    int add() {
+       int add = a+b;
+       return add;
+    }
+    int sub (){
+        int sub=a-b;
+        return sub;
+    }
+    int mul (){
+        int mul=a*b;
+        return mul;
 
     }
-    int sub {
-        sub=a-b
+};
 
-    }
-    int mul {
-        mul=a*b
-
-    }
-}
 int main()
-
 {
     calculator att1;
-    cout << "add " << att1.add << endl;
-    cout << "sub " << att1.sub << endl;
-    cout << "mul " << att1.mul << endl;
+    cout << "add " << att1.add() << endl;
+    cout << "sub " << att1.sub() << endl;
+    cout << "mul " << att1.mul() << endl;
     return 0;
 }
