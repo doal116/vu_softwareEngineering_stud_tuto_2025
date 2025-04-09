62a51893aec88f9740610d0984141070d3051e3a
AminBadalzade <amin.badalzade@sa.stud.vu.lt>
2025-04-09 20:54:02 +0300

    Alex's salary

--- main.cpp
+++ main.cpp
@@ -3,13 +3,40 @@
 
 using namespace std;
 
-int Amin(int a,int b){
-    return a+b;
-}
+struct worker {
+    string fullName;
+    string position;
+    double salary;
 
-int main()
-{
+    worker(string fullName, string position, double salary) {
+        this->fullName = fullName;
+        this->position = position;
+        this->salary = salary;
+    }
 
-    cout << "Hello my name is Amin" << endl;
+    void info() {
+        cout << "Worker: " << fullName << " at " << position << " position " << "earns " << salary << "$\n";
+    }
+
+    void afterModification() {
+        addMoney();
+        cout << fullName << " will earn " << salary << "$ in a month\n";
+    }
+
+    double addMoney() {
+        return salary += 1000;
+    }
+
+    void yearlySalary() {
+        cout << "Yearly salary is: " << salary * 12 << "$" << endl;
+    }
+};
+
+int main() {
+    worker Alex("Alex Bulganin", "Fullstack developer", 3000);
+    Alex.info();
+    Alex.afterModification();
+    Alex.yearlySalary();
+    
     return 0;
 }
