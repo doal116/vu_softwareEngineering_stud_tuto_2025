4b2d0d350c8ebae3fc81fb5413b968c5198a6c99
doal116 <bulganinbralex@yahoo.com>
2025-04-09 20:40:40 -0100

    struct idea and concept

--- main.cpp
+++ main.cpp
@@ -1,17 +1,47 @@
 #include <iostream>
 #include <string>
-
+#include <vector>
 using namespace std;
 
+struct Identity{
+    int age;
+    string firstName;
+    string lastName;
 
-int Sean(int a, int b){
-    cout << "I did this" << endl;
-    return a + b;
-}
+    int giveYearOfBirth(){
+        return 2025 - age;
+    };
+};
+struct GTA_Player{
+    private:
+        double money=1;
+        void addMoney(){
+            money++;
+        };
+    public:
+        void displayMoney(){
+            cout << money<< endl;
+        };
+};
 
 int main()
 {
-    cout << "Hello my is Alex" << endl;
+    vector<Identity>listOfPerson;
+
+    Identity person1;
+    person1.age=12;
+    person1.firstName="alex" ;
+    person1.lastName="bob";
+
+    // cout << person1.giveYearOfBirth()<<endl;
+    GTA_Player batuhan;
+    batuhan.displayMoney();
+    // vector<int>listOfPersonAge;
+    // vector<string>listOfPersonfirstnamee;
+    // vector<string>listOfPersonlastnamee;
+    // int person1age=12;
+    // string person1FirstName="alex";
+    // string person1LastName="bob";
     return 0;
 }
 
