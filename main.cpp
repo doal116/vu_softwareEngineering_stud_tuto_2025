60f50cebfcb84a5b930443f8c7b647b7db78aba9
Fuad Mahmud Shad <fuademrite@gmail.com>
2025-04-09 21:19:05 +0300

    Updated Changes

--- main.cpp
+++ main.cpp
@@ -6,44 +6,47 @@ using namespace std;
 
 
 struct calculator {
-     
+
     int a, b;
 
-    int add (int a, int b) {
+    int add(int a, int b) {
         return a + b;
     }
 
-    int substract (int a, int b) {
-        if ( a > b) {
+    int substract(int a, int b) {
+        if (a > b) {
             return a - b;
         }
 
-        if ( b > a) {
-           int temp = a;
+        if (b > a) {
+            int temp = a;
             a = b;
             b = temp;
 
-            return b - a;
+            return a - b;
         }
-        return 0;
+        if (a == b) {
+            return a - b;
+        }
+            
     }
 
 
-    int mul ( int a, int b) {
+    int mul(int a, int b) {
         return a * b;
     }
 
 };
 
-int main () {
+int main() {
     int a, b;
     cout << "Please give value to Integer a and b: " << endl;
     cin >> a >> b;
 
     calculator PartOne;
-    cout << "The addition of the two inetegrs " << a << " and" << b << " is" << PartOne.add(a, b) << endl;
-    cout << "The substraction of the two inetegrs " << a << " and" << b << " is" << PartOne.substract(a, b) << endl;
-    cout << "The multiplication of the two inetegrs " << a << " and" << b << " is" << PartOne.mul(a, b) << endl;
+    cout << "The addition of the two inetegrs " << a << " and " << b << " is " << PartOne.add(a, b) << endl;
+    cout << "The substraction of the two inetegrs " << a << " and " << b << " is " << PartOne.substract(a, b) << endl;
+    cout << "The multiplication of the two inetegrs " << a << " and " << b << " is " << PartOne.mul(a, b) << endl;
 
     return 0;
 
