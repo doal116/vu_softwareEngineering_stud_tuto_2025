a5afa5f6affdeec3e4ce7f5258303c6bada79fac
doal116 <bulganinbralex@yahoo.com>
2025-04-09 19:41:32 -0100

    changes comming from alex's computer

--- main.cpp
+++ main.cpp
@@ -7,7 +7,9 @@ int Sean(int a, int b){
     cout << "I did this" << endl;
     return a + b;
 }
-
+int sub(int a, int b){
+    return a - b;
+}
 int main()
 {
     cout << "hello world what up :)" << endl;
