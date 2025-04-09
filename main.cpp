d2a06d001f2eab2c4c3a480fe3e80d36b04b6503
IkramZulbalayev <ikram.zulbalayev@sa.stud.vu.lt>
2025-04-09 21:16:15 +0300

    Struct

--- main.cpp
+++ main.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
+struct Calculator {
+    double a, b;
+    double add() {
+        return a + b;
+    }
 
-int Amin(int a, int b){
-    cout << "result is: ";
-    return a+b;
-}
+    double sub() {
+        return a - b;
+    }
+
+    double mult() {
+        return a * b;
+    }
+
+};
 
 int main()
 {
-    cout << "Hello my name is: Ikram" << endl;
+    Calculator calculator;
+   
+    cin >> calculator.a >> calculator.b;
+
+    cout << "Addition of two numbers: " << calculator.add() << '\n';
+    cout << "Substraction of two numbers: " << calculator.sub() << '\n';
+    cout << "Multiplication of two numbers: " << calculator.mult() << '\n';
+
     return 0;
 }
