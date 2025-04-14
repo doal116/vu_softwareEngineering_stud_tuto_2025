d982c73ba2bea995d7f68466ff78425ce7e75a76
sithija <hsithija2005@gmail.com>
2025-04-14 19:48:28 +0530

    bank

--- main.cpp
+++ main.cpp
@@ -19,17 +19,20 @@ namespace bank{
     }
     
     namespace internal{
-        struct assistantmanager{
+        struct manager{
             string bankid;
             string firstname;
             string lastname;
         };
-    }}
+    }
+}
 
-namespace bank:internal{
-    struct manager{
-        string bankid;
-        string firstname; 
-        string lastname;
-    };
+
+int main() {
+    bank::internal::manager person1;
+    person1.bankid="sith12";
+    person1.firstname="sithija";
+    person1.lastname="appuhamy";
+ cout<<"the id of the manager is "<<person1.bankid<<endl;
+ cout<<"the name of the manage is "<<person1.firstname<<" "<<person1.lastname<<endl;
 }
