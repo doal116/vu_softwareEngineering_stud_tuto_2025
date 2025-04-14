3a811ddcb1caad5b65e00850973f25095859610d
doal116 <bulganinbralex@yahoo.com>
2025-04-14 17:07:59 -0100

    ignore main

--- main.cpp
+++ main.cpp
@@ -3,32 +3,36 @@
 
 using namespace std;
 
-namespace bank{
+namespace bank
+{
 
-namespace internal{
+    namespace internal
+    {
 
-        struct manager{
-        int age;
-        string firstname;
-        string lastname;
-    };
-}
+        struct manager
+        {
+            int age;
+            string firstname;
+            string lastname;
+        };
+    }
 
-namespace external{
+    namespace external
+    {
 
-    struct teller{
-        int workerid;
-        string firstname;
-        string lastname;
- 
-    };
+        struct teller
+        {
+            int workerid;
+            string firstname;
+            string lastname;
+        };
 
-    struct client{
-        int accountnumber;
-        string bank;
-        string firstname;
-        string lastname;
- 
+        struct client
+        {
+            int accountnumber;
+            string bank;
+            string firstname;
+            string lastname;
         };
     }
 }
@@ -39,7 +43,6 @@ int main()
     manager.age = 50;
     manager.lastname = "george";
     manager.firstname = "tsertsvadze";
-    
 
     cout << manager.firstname << manager.lastname << manager.age << endl;
     return 0;
