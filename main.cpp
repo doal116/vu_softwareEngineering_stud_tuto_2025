7c9511f36ae65b00af232c839388956de1ded9be
aleqsandresamadashvili <151573307+aleqsandresamadashvili@users.noreply.github.com>
2025-04-14 16:40:16 +0300

    aleqs

--- main.cpp
+++ main.cpp
@@ -3,6 +3,33 @@
 
 using namespace std;
 
+namespace internal{
+
+        struct manager{
+        int age;
+        string firstname;
+        string lastname;
+    }
+}
+
+namespace actor{
+
+    struct teller{
+        int workerid;
+        string firstname;
+        string lastname;
+ 
+    }
+    
+    struct client{
+        int accountnumber;
+        string bank;
+        string firstname;
+        string lastname;
+ 
+    }
+}
+
 int main()
 {
     cout << "alexandre" << endl;
