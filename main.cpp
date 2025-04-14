d2ecd2553b829a4c2acb72804abce6d76225fe24
Cercva1 <giorgi.tsertsvadze@sa.stud.vu.lt>
2025-04-14 16:40:16 +0300

    BAnkSys

--- main.cpp
+++ main.cpp
@@ -3,7 +3,43 @@
 #include <vector>
 
 using namespace std;
-namespace Game
+namespace actor{
+    struct client
+    {
+        string BankAccNumber;
+        string firstname;
+        string lastname;
+
+        string clientInfo(){
+            cout<<"Client " firstname, lastname " has recived his Bank Account number " BankAccNumber<<endl;
+        }
+
+        struct teller
+        {
+            int workerId;
+            string firstname;
+            string lastname;
+
+
+        };
+        
+    };
+    
+}
+
+
+namespace internal{
+
+    struct maneger
+    {
+        int id;
+        string privegelLevel;
+        string firstname;
+        string lastname;
+    };
+    
+
+}
 
 int main(){
     cout << "Hello world" << endl;
