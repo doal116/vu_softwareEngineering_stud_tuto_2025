404f2b515172178e261cc5b3fe11f68c89f535c6
sithija <hsithija2005@gmail.com>
2025-04-14 19:38:00 +0530

    bank4

--- main.cpp
+++ main.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
+namespace bank{
+    namespace external{
+        struct client{
+            string bankaccountnum;
+            string firstname;
+            string lastname;
+        };
+    
+        struct teller{
+            string bankid;
+            string firstname;
+            string lastname;
+        };
+    }
+    
+    namespace internal{
+        struct assistantmanager{
+            string bankid;
+            string firstname;
+            string lastname;
+        };
+    }}
 
-int main()
-{
-    cout << "hello world what up :)" << endl;
-    return 0;
+namespace bank:internal{
+    struct manager{
+        string bankid;
+        string firstname; 
+        string lastname;
+    };
 }
