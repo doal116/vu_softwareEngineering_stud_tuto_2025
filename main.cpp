17c2d2a4285c716d074f6fd4e66f8a6114846027
Cercva1 <giorgi.tsertsvadze@sa.stud.vu.lt>
2025-04-14 17:02:02 +0300

    Calling structs

--- main.cpp
+++ main.cpp
@@ -3,46 +3,65 @@
 #include <vector>
 
 using namespace std;
-namespace actor{
-    struct client
-    {
-        string BankAccNumber;
-        string firstname;
-        string lastname;
-
-        void clientInfo(){
-            cout<<"Client " firstname, lastname " has recived his Bank Account number " BankAccNumber<<endl;
-        }
-
-    };
-    struct teller
-    {
-        int workerId;
-        string firstname;
-        string lastname;
-
-
-    };
+namespace Bank{
+    
+    namespace external{
+        struct client
+        {
+            string BankAccNumber;
+            string firstname;
+            string lastname;
+    
+            void clientInfo(){
+                cout<<"Client " firstname, lastname " has recived his Bank Account number " BankAccNumber<<endl;
+            }
+    
+        };
+        struct teller
+        {
+            int workerId;
+            string firstname;
+            string lastname;
     
     
-}
-
-
-namespace internal{
-
-    struct maneger
-    {
-        int id;
-        string privegelLevel;
-        string firstname;
-        string lastname;
-    };
+        };
+        
+        
+    }    
     
-
+    namespace internal{
+    
+        struct maneger
+        {
+            int id;
+            int privegelLevel;
+            string firstname;
+            string lastname;
+        };
+        
+    
+    }
+    
 }
 
 int main(){
-    cout << "Hello world" << endl;
+
+   Bank::external::client client;
+   client.BankAccNumber = "GETB0076TB60000";
+   client.firstname = "Sean";
+   client.lastname = "Dewe";
+
+   Bank::internal::maneger manager;
+   manager.id = 5;
+   manager.privegelLevel = 100;
+   manager.firstname = "George";
+   manager.lastname = "Tsertsvadze";
+
+   cout<<"Manager " manager.firstname<<" "<<manager.lastname<<" with id "<<id<<" has PRVGLVL on "
+   <<manager.privegellevel<<"%"<<endl;
+
+
+
     return 0;
 }
 
