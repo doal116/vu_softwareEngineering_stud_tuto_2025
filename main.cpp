867bfb68facaf4031944ae85a206c85b65145d2f
doal116 <bulganinbralex@yahoo.com>
2025-04-16 19:04:51 -0100

    challenge complete

--- main.cpp
+++ main.cpp
@@ -19,60 +19,57 @@ namespace CalcTask
             this->b = b;
         }
         // Greatest Common Divisor 48 18 gcd = 6 : name gcd | a int b int
-        int gcd(){
-            int a = a, b = b;
-            while (b != 0){
+        int gcd()
+        {
+            int a = this->a, b = this->b;
+            while (b != 0)
+            {
                 int temp = b;
                 b = a % b;
                 a = temp;
             }
             return a;
-            
         }
 
-
         // Exponentiation 2^3 = 8: name exp | a base b exponent
-        int exp(){
+        int exp()
+        {
             int result = 1;
-            for (int i = 0; i < b; i++){
-                result *= a;
+            for (int i = 0; i < this->b; i++)
+            {
+                result *= this->a;
             }
             return result;
         }
 
         // Factorial 4! = 4x3x2x1 = 24: name fact | only use a. b can be eqaual to 0
-        int fact(){
-            if (a<0)return -1;
-            int result =1;
-            for (int i = 2; i <= a; i++){
-                result *i;
+        int fact()
+        {
+            if (this->a < 0)
+                return -1;
+            int result = 1;
+            for (int i = 2; i <= this->a; i++)
+            {
+                result *= i;
             }
+            return result;
         }
 
-
         // Least Common Multiple 12 18 LCM = 36 : name lcm | a int b int
-        int lcm(){
-            return (a*b)/ gcd();
+        int lcm()
+        {
+            return (this->a * this->b) / gcd();
         }
     };
 
 }
 int main()
 {
-    CalcTask::Calculator calc1(1, 2);
-    cout << calc1.a << calc1.b << endl;
-    /*
     // unicodes
-    const string GREEN = "\033[32m";
-    const string RED = "\033[31m";
-    const string RESET = "\033[0m";
-
-    const string CHECK = "\u2714";
-    const string CROSS = "\u274C";
-
     //===========
     // TestOne
     //===========
+    /*
     vector<int> test1Expected = {3, 5, 7, 6, 7, 12, 15, 20};
     vector<CalcTask::Calculator> test1 = {
         CalcTask::Calculator(6, 9),   // GCD is 3
@@ -88,9 +85,9 @@ int main()
     for (int i = 0; i < test1.size(); i++)
     {
         if (test1[i].gcd() == test1Expected[i])
-            cout << GREEN << CHECK << RESET << "gcd of: " << test1[i].a << " | " << test1[i].b << " is " << test1[i].gcd();
+            cout  << "gcd of: " << test1[i].a << " | " << test1[i].b << " is " << test1[i].gcd()<<endl;
         else
-            cout << RED << CROSS << RESET << "gcd of: " << test1[i].a << " | " << test1[i].b << " is not " << test1[i].gcd();
+            cout << "gcd of: " << test1[i].a << " | " << test1[i].b << " is not " << test1[i].gcd()<<endl;
     }
 
     //===========
@@ -106,13 +103,14 @@ int main()
         CalcTask::Calculator(6, 2),
         CalcTask::Calculator(2, 7),
         CalcTask::Calculator(3, 4)};
-    for (int i = 0; i < test1.size(); i++)
+    for (int i = 0; i < test2.size(); i++)
     {
         if (test2[i].exp() == test2Expected[i])
-            cout << GREEN << CHECK << RESET << "exp of: " << test1[i].a << " to the power of " << test1[i].b << " is " << test1[i].exp();
+            cout << "exp of: " << test2[i].a << " to the power of " << test2[i].b << " is " << test2[i].exp()<<endl;
         else
-            cout << RED << CROSS << RESET << "exp of: " << test1[i].a << " to the power of " << test1[i].b << " is not " << test1[i].exp();
+            cout  << "exp of: " << test2[i].a << " to the power of " << test2[i].b << " is not " << test2[i].exp()<<endl;
     }
+
 
     //===========
     // TestThree
@@ -128,12 +126,12 @@ int main()
         CalcTask::Calculator(5, 0),
         CalcTask::Calculator(6, 0),
         CalcTask::Calculator(8, 0)};
-    for (int i = 0; i < test1.size(); i++)
+    for (int i = 0; i < test3.size(); i++)
     {
         if (test3[i].fact() == test3Expected[i])
-            cout << GREEN << CHECK << RESET << "fact of: " << test3[i].a << " is " << test3[i].fact();
+            cout << "fact of: " << test3[i].a << " is " << test3[i].fact()<<endl;
         else
-            cout << RED << CROSS << RESET << "fact of: " << test3[i].a << " is not " << test3[i].fact();
+            cout << "fact of: " << test3[i].a << " is not " << test3[i].fact()<<endl;
     }
 
     //===========
@@ -151,12 +149,13 @@ int main()
         CalcTask::Calculator(9, 15),
         CalcTask::Calculator(7, 8)};
 
-    for (int i = 0; i < test1.size(); i++)
+    for (int i = 0; i < test4.size(); i++)
     {
         if (test4[i].lcm() == test4Expected[i])
-            cout << GREEN << CHECK << RESET << "fact of: " << test4[i].a << " is " << test4[i].lcm();
+            cout << "lcm of: " << test4[i].a << " | "<< test4[i].b << " is " << test4[i].lcm() << endl;
         else
-            cout << RED << CROSS << RESET << "fact of: " << test4[i].a << " is not " << test4[i].lcm();
-    }*/
+            cout << "lcm of: " << test4[i].a << " | "<< test4[i].b << " is not " << test4[i].lcm() << endl;
+    }
     return 0;
+    */
 }
