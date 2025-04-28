6ac577b050f59412a19b95eb3df6a5e8c6c2fdd5
seandewe <dewesean@gmail.com>
2025-04-28 20:01:57 +0300

    sean task worker

---
+++ Step1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Step1
+{
+    public struct Worker{
+
+        public string firstName;
+        public int yearOfBirth;
+        public int currentYear;
+
+        public Person(string firstname, int yob, int year){
+            this.firstName = firstname;
+            this.yearOfBirth = yob;
+            this.currentYear = year;
+        }
+        public void calculateAge(){
+            
+        }
+    }
+    public class HelloWorld
+    {
+        public static void Main(string[]args)
+        {
+           
+        }
+    }
+}
+
+
+
